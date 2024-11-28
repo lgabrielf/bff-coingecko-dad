0b6dad0c787567eef8874885495c9d3c3a4e225f
Lucas Ferreira <lucas.ferreira@geowellex.com>
2024-11-27 21:43:53 -0300

    add: auth and startup route

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from src.controllers import cryptocurrency
 from src.controllers import market_summary
 from src.controllers import price_history
+from src.controllers import user
 from src.config.config import settings
+from src.config.database import create_tables
 
 app = FastAPI(
     title=settings.FASTAPI_TITLE,
@@ -12,7 +14,11 @@ app = FastAPI(
     docs_url=('/docs' if settings.FASTAPI_DEBUG else None),
     redoc_url=('/redocs' if settings.FASTAPI_DEBUG else None),
 )
-
+@app.on_event("startup")
+async def startup_event():
+    print("Iniciando a aplicação...")
+    await create_tables()
+    
 app.include_router(
     cryptocurrency.router,
     tags=['Top Cryptocurrencies'],
@@ -26,5 +32,10 @@ app.include_router(
 app.include_router(
     price_history.router,
     tags=['Cryptocurrency History'],
+    prefix=f'/{settings.FASTAPI_PREFIX}',
+)
+app.include_router(
+    user.router,
+    tags=['Authentication'],
     prefix=f'/{settings.FASTAPI_PREFIX}',
 )
